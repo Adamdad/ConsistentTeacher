4cbc989c351a3baa72294716bc72a168aa6202fb
Adamdad <787707188@qq.com>
2023-03-20 17:10:12 +0800

    _delete_=True

--- configs/consistent-teacher/consistent_teacher_r50_fpn_coco_180k_10p_2x8_swav.py
+++ configs/consistent-teacher/consistent_teacher_r50_fpn_coco_180k_10p_2x8_swav.py
@@ -1,11 +1,12 @@
 _base_ = './consistent_teacher_r50_fpn_coco_180k_10p_2x8.py'
 
-fp16 = dict(_delete_=True,
-            loss_scale=512.)
+# fp16 = dict(_delete_=True,
+#             loss_scale=512.)
 model = dict(
     backbone=dict(
         # frozen_stages=1,
         # norm_cfg=dict(type='SyncBN', requires_grad=True),
         # norm_eval=False,
-        init_cfg=dict(type='Pretrained', 
+        init_cfg=dict(_delete_=True,
+                      type='Pretrained', 
                       checkpoint='/Checkpoint/yangxingyi/Pretrained/swav_800ep_pretrain.pth.tar')))
