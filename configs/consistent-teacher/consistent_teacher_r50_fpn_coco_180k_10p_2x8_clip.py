daec54b0a75f3dfcd6d3feadc7c4c6ef03cbf7ce
Adamdad <787707188@qq.com>
2023-03-20 15:49:56 +0800

    aa

--- configs/consistent-teacher/consistent_teacher_r50_fpn_coco_180k_10p_2x8_clip.py
+++
@@ -1,7 +0,0 @@
-_base_ = './consistent_teacher_r50_fpn_coco_180k_10p_2x8.py'
-
-model = dict(
-    backbone=dict(
-        init_cfg=dict(type='Pretrained', 
-                      prefix='visual.',
-                      checkpoint='/Checkpoint/yangxingyi/Pretrained/clip_RN50.pth')))
