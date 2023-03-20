945495659d9f751ac989d195e4cf63d2bc41d6ef
Adamdad <787707188@qq.com>
2023-03-20 15:55:09 +0800

    norm_cfg=dict(type='SyncBN', requires_grad=True),

--- configs/consistent-teacher/consistent_teacher_r50_fpn_coco_180k_10p_2x8_swav.py
+++ configs/consistent-teacher/consistent_teacher_r50_fpn_coco_180k_10p_2x8_swav.py
@@ -2,5 +2,7 @@ _base_ = './consistent_teacher_r50_fpn_coco_180k_10p_2x8.py'
 
 model = dict(
     backbone=dict(
+        frozen_stages=0,
+        norm_cfg=dict(type='SyncBN', requires_grad=True),
         init_cfg=dict(type='Pretrained', 
                       checkpoint='/Checkpoint/yangxingyi/Pretrained/swav_800ep_pretrain.pth.tar')))
