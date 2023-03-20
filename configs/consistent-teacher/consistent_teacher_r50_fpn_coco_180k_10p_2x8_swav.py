470b64cbdf5195af0513bc3ccba628a76c187f9f
Adamdad <787707188@qq.com>
2023-03-20 16:16:18 +0800

    frozen_stages=1,

--- configs/consistent-teacher/consistent_teacher_r50_fpn_coco_180k_10p_2x8_swav.py
+++ configs/consistent-teacher/consistent_teacher_r50_fpn_coco_180k_10p_2x8_swav.py
@@ -2,7 +2,7 @@ _base_ = './consistent_teacher_r50_fpn_coco_180k_10p_2x8.py'
 
 model = dict(
     backbone=dict(
-        frozen_stages=0,
+        frozen_stages=1,
         norm_cfg=dict(type='SyncBN', requires_grad=True),
         norm_eval=False,
         init_cfg=dict(type='Pretrained', 
