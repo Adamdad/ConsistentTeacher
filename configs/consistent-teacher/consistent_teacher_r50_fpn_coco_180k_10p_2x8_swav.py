e6f2c8a107eb97ae71b7eff7c4a8ad1086c05d06
Adamdad <787707188@qq.com>
2023-03-27 15:38:44 +0800

    _delete_=True,

--- configs/consistent-teacher/consistent_teacher_r50_fpn_coco_180k_10p_2x8_swav.py
+++ configs/consistent-teacher/consistent_teacher_r50_fpn_coco_180k_10p_2x8_swav.py
@@ -1,6 +1,7 @@
 _base_ = './consistent_teacher_r50_fpn_coco_180k_10p.py'
 
-fp16 = dict(loss_scale=512.)
+fp16 = dict(_delete_=True,
+            loss_scale=512.)
 model = dict(
     backbone=dict(
         frozen_stages=0,
