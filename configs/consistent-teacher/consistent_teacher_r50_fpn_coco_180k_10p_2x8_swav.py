fd80cc82d928407c22fab9a5bd3f64055e686a43
Adamdad <787707188@qq.com>
2023-03-27 15:34:31 +0800

    fp16 = dict(loss_scale=512.)

--- configs/consistent-teacher/consistent_teacher_r50_fpn_coco_180k_10p_2x8_swav.py
+++ configs/consistent-teacher/consistent_teacher_r50_fpn_coco_180k_10p_2x8_swav.py
@@ -1,10 +1,11 @@
 _base_ = './consistent_teacher_r50_fpn_coco_180k_10p.py'
 
+fp16 = dict(loss_scale=512.)
 model = dict(
     backbone=dict(
-        # frozen_stages=1,
+        frozen_stages=0,
         # norm_cfg=dict(type='SyncBN', requires_grad=True),
-        # norm_eval=False,
+        norm_eval=False,
         init_cfg=dict(_delete_=True,
                       type='Pretrained', 
                       checkpoint='/Checkpoint/yangxingyi/Pretrained/swav_800ep_pretrain.pth.tar')))
